05655e52aeb7e37bf9ed5638e4885a066daa89e4
omokehinde <engr.omokehinde@gmail.com>
2022-01-17 15:33:16 +0100

    Add Expense Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Stack, Container, Button } from "react-bootstrap";
 import AddBudgetModal from "./components/AddBudgetModal";
+import AddExpenseModal from "./components/AddExpenseModal";
 import BudgetCard from "./components/BudgetCard";
 import { useBudget } from "./context/BudgetContext";
 
@@ -15,7 +16,7 @@ function App() {
       <Stack direction="horizontal" gap={2} className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary" onClick={handleshow}>Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
+        <Button variant="outline-primary" onClick={handleshow} >Add Expense</Button>
       </Stack>
       <div style={{ display:"grid", 
         gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", 
@@ -30,6 +31,7 @@ function App() {
         </div>
     </Container>
     <AddBudgetModal show={show} handleClose={handleClose}  />
+    <AddExpenseModal show={show} handleClose={handleClose}  />
     </>
   );
 }
